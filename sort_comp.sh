67cb2eec7c3005331796868222299ffa4d02e39d
cyril joseph <cyrilvj94@gmail.com>
2021-01-20 14:25:48 +0530

    Sorted array in descending

--- sort_comp.sh
+++ sort_comp.sh
@@ -1,16 +1,49 @@
 #! /bin/bash
 
+#function to sort array
+function sortArrayDesc
+{
+array=$1
+n=${#array[@]}
+for((i=0;i<$n;i++))
+do
+        for((j=$i;j<$n;j++))
+        do
+                if [ ${array[i]} -lt ${array[j]} ]
+                then
+                        #swap
+                        temp=${array[i]}
+                        array[$i]=${array[j]}
+                        array[$j]=$temp
+                fi
+        done
+done
+
+echo   ${array[@]}
+
+}
+
+
 read -p"Enter the numbers" a b c 
 num1=$((a+b*c))
 num2=$((a*b+c))
 num3=$((c+ a/b))
 num4=$((a%b + c))
 declare -A dict
-declare -a arr
+declare -a array
 dict['num1']=$num1
 dict['num2']=$num2
 dict['num3']=$num3
 dict['num4']=$num4
 
-arr=${dict[@]}
-echo ${arr[@]}
+j=0
+for value in ${dict[@]}
+do
+	array[$j]=$value
+	j=$((j+1))
+done
+
+echo ${array[@]}
+sorted=$(sortArrayDesc $array)
+echo Sorted Array Descening ${sorted[@]}
+
